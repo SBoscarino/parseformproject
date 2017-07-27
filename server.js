fc81642ba9c8de5bac234337711b80b790f60486
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-07-27 10:47:54 -0500

    second commit, making progress

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.set('views', __dirname + '/views');
 
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.post('/signUp', function(req, res){
+  res.render();
+})
+
 app.listen(4500, function(){
   console.log("Form app is listening on port 4500");
 })
